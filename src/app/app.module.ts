14b0225458d77d81d25ef8e94cae58408cadc665
Earl Hans Genoso <earlhansg@gmail.com>
2020-03-09 16:28:25 +0800

    #4 Update README and add Hall Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './root/container/app.component';
 import { HeaderComponent } from './root/components/header/header.component';
 import { HeroesComponent } from './root/components/heroes/heroes.component';
+import { HallComponent } from './root/components/hall/hall.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroesComponent
+    HeroesComponent,
+    HallComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/root/components/hall/hall.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hall',
+  templateUrl: './hall.component.html',
+  styleUrls: ['./hall.component.scss']
+})
+export class HallComponent {}
